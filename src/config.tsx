79c60a69c065e1b2bc03af8e27047120e36b0914
Andrey Sluzhivoy <andrey.sluzhivoy@d-one.ai>
2024-02-13 09:03:04 +0100

    package config

--- src/config.tsx
+++ src/config.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Settings from './components/Settings.tsx';
+import './style.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
